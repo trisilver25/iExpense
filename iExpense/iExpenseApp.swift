2da4c98873c8250ddc2410a75aea2abaa1a3cacb
Tristin Smith <tsmithassoc@gmail.com>
2024-06-18 19:34:11 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Tristin Smith on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
